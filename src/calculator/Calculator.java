41f29d027790a470bc27a5f47994807bfa0ea55f
shuaihuaiyi <932495000@qq.com>
2016-09-19 22:18:44 +0800

    ini

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,156 @@
+package calculator;
+
+import java.util.*;
+
+public class Calculator
+{
+	//单项式类
+	private static class Monomial
+	{
+		//表达式中的元素
+		Double coefficient;//系数
+		HashMap<String, Integer> vars;//变量，保存为变量名->次数的映射
+		
+		Monomial(String monomial)
+		{
+			coefficient = 1.0;
+			vars = new HashMap<String, Integer>();
+			if(monomial.charAt(0) == '-')//处理负号
+			{
+				coefficient = -1.0;
+				monomial = monomial.substring(1);
+			}
+			String[] factors = monomial.split("\\*|(?<=[0-9])(?=[A-Za-z])");
+			/*正常来讲，factor只能有以下几种情况：
+			 *1、变量，如x，boo
+			 *2、数字，如1, 12，15.2
+			 *3、幂式，如x^3，value^5
+			 */
+			if(factors.length == 1)
+				;//FIXME Error!!
+			else
+			{
+				for (String factor : factors)
+				{
+					if(factor.matches("^[A-Za-z]+$"))//变量
+					{
+						vars.put(factor,(((vars.get(factor)==null) ? 0 : (vars.get(factor)))+1));
+						
+					}
+					else if(factor.matches("^[0-9]+[\\.]?[0-9]+$"))//数字
+					{
+						coefficient *= Double.valueOf(factor);
+					}
+					else if(factor.matches("^[A-Za-z]+\\^[1-9][0-9]*$"))//幂式
+					{
+						String[] pair = factor.split("(?<=[A-Za-z]+)\\^(?=[1-9][0-9]*)");
+						if(pair.length == 1)
+							;//FIXME Error!!
+						else
+						{
+							String var = pair[0];//变量名
+							int index = Integer.valueOf(pair[1]);//指数
+							vars.put(var,(((vars.get(var)==null) ? 0 : (vars.get(var)))+index));
+						}
+						
+					}
+					else
+					{
+						;//FIXME Error!!
+					}
+				}
+			}
+		}
+		
+	}
+	//将字符串表达式转换成自定义数据类型
+	static void expression(String input)
+	{
+		ArrayList <Monomial> exp = new ArrayList <Monomial> ();//表达式
+		//FIXME 异常处理呢？？？@lqf
+		String fixedInput = input.replaceAll("\\s+","");
+		String[] monomials = fixedInput.split("(?=\\+|-)");
+		for(String monomial : monomials)
+		{
+			exp.add(new Monomial(monomial));
+		}
+	}
+	//按照输入的值对表达式进行计算
+	//注意加法的处理
+	static void simplify(String input)
+	{
+		String[] assigns = input.substring(9).split(" ");//FIXME 开始时有空格则无法处理
+		for(String assign : assigns)
+		{
+			String[] temp = assign.split("=");
+			if(temp.length == 1)
+			{
+				//TODO 异常处理
+			}
+			else
+			{
+				String var = temp[0];
+				Double value = Double.valueOf(temp[1]);
+				//TODO 
+			}
+		}
+	}
+	//对表达式求导
+	static void derivative(String input)
+	{
+		//TODO 补全这个函数
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		//HashMap<String, Integer> vars = new HashMap<String, Integer>();
+		Scanner scan = new Scanner(System.in);
+		while(true)
+		{
+			String input = scan.nextLine();
+			if(input.equals("!"))
+			{
+				scan.close();
+				System.exit(0);
+			}
+			else
+			{
+				//System.out.println(input.lookingAt("asc"));
+				String[] splitedInputs = input.substring(9).split(" ");
+				for(String monomial:splitedInputs)
+				{
+					System.out.println(monomial);
+				}
+			}
+		}
+		/*while(true)
+		{
+			if(scan.hasNextLine())
+			{
+				String input = scan.nextLine();
+				if(input.matches("^[A-Za-z0-9 -][A-Za-z0-9*^]*[([ +-]{1})([A-Za-z0-9*^]+)]*$"))
+				{
+					string=input;
+					//TODO 
+					System.out.println(input);
+				}
+				else if(input.matches("!simplify[ A-Za-z0-9=]+$") )
+				{
+					simplify(string,input);
+				}
+				else if(input.matches("!d\\/d[A-Za-z]+$"))
+				{
+					derivative(string,input);
+				}
+				else
+				{
+					System.out.println("Error input!Please try it again!");
+					System.exit(0);
+				}
+			}
+			
+		}*/
+	}
+
+}
